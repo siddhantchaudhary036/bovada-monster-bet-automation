7b302fc61a0a69b9f75cc4a625e0d796e6c57ab7
siddhantchaudhary036 <160422817+siddhantchaudhary036@users.noreply.github.com>
2024-09-20 00:46:46 -0500

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,4 @@
+monster_auth_token = '' # This you have to get from the developer tools when you are signed into monster
+monster_user_id = '' # This you have to get from the developer tools when you are signed into monster
+password = '' # This is your bovada password
+username = '' # This is your bovada username
